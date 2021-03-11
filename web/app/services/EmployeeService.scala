26798647b154895f3f9cc7a9071a505282fdadde
Shad Amez <shad.amezng@gmail.com>
2021-03-11 15:32:41 +0530

    temp method

--- web/app/services/EmployeeService.scala
+++ web/app/services/EmployeeService.scala
@@ -7,6 +7,8 @@ import scala.concurrent.{ExecutionContext, Future}
 
 trait EmployeeService {
   def listEmployees()(implicit ec: ExecutionContext): Future[Seq[EmpDetail]]
+
+  def addEmployee(emp: EmpDetail)(implicit ec: ExecutionContext): Future[Long]
 }
 
 class EmployeeServiceImpl extends EmployeeService {
@@ -15,10 +17,14 @@ class EmployeeServiceImpl extends EmployeeService {
     List(EmpDetail("Shad", 11, "..."))
   }
 
+  override def addEmployee(emp: EmpDetail)(implicit ec: ExecutionContext): Future[Long] = ???
+
 }
 
 class InMemoryEmployeeServiceImpl extends EmployeeService {
   override def listEmployees()(implicit ec: ExecutionContext): Future[Seq[EmpDetail]] = Future {
     List(EmpDetail("From Memory Shad", 101010, "..."))
   }
+
+  override def addEmployee(emp: EmpDetail)(implicit ec: ExecutionContext): Future[Long] = ???
 }
